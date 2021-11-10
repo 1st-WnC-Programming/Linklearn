e595c49696b25550eab13b2f9f174c01f3427b53
jeonbyeongmin <qudals7613@gmail.com>
2021-11-11 01:30:26 +0900

    update auth

--- front-end/src/Components/App.js
+++ front-end/src/Components/App.js
@@ -1,14 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import AppRouter from './Router';
 import GlobalStyles from './GlobalStyles';
 import Footer from './Footer';
+import { authService } from '../fbase';
 
 const App = () => {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <GlobalStyles />
-      <AppRouter />
-      <Footer />
-      <footer>&copy; {new Date().getFullYear()} Linklearn</footer>
+      {init ? (
+        <>
+          <GlobalStyles />
+          <AppRouter isLoggedIn={isLoggedIn} />
+          <Footer />
+          <footer>&copy; {new Date().getFullYear()} Linklearn</footer>
+        </>
+      ) : (
+        'Loading...'
+      )}
     </>
   );
 };
