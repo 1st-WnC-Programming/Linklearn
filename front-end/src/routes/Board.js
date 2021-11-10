fd226f5af482ff2613325a828d9e7e1413a82320
bae-sh <hyeon1293@gmail.com>
2021-11-11 00:36:30 +0900

    make board container

--- front-end/src/routes/Board.js
+++ front-end/src/routes/Board.js
@@ -1,4 +1,29 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+`;
+const SearchBox = styled.div`
+  margin: auto;
+  width: 90%;
+  height: 50px;
+  background-color: lightblue;
+  margin-bottom: 50px;
+`;
+const TeacherBox = styled.div`
+  background-color: white;
+  width: 350px;
+  height: 200px;
+  border: 1px solid lightgray;
+`;
 const Board = () => {
-  return <div></div>;
+  return (
+    <div className='inner'>
+      <SearchBox>123</SearchBox>
+      <Container>
+        <TeacherBox>123</TeacherBox>
+      </Container>
+    </div>
+  );
 };
 export default Board;
