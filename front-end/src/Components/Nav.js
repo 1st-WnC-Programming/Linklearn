07f3eba17212bc0b15fd8e61d1db17853f618150
jeonbyeongmin <qudals7613@gmail.com>
2021-11-10 21:47:58 +0900

    update router

--- front-end/src/Components/Nav.js
+++ front-end/src/Components/Nav.js
@@ -1,19 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>
-          <Link to="/Board">Board</Link>
+          <Link to='/Board'>Board</Link>
         </li>
-
         <li>
-          <Link to="/Login">Login</Link>
+          <Link to='/Auth'>Auth</Link>
+        </li>
+        <li>
+          <Link to='/TeacherList'>TeacherList</Link>
         </li>
       </ul>
     </nav>
--- front-end/src/Components/Router.js
+++ front-end/src/Components/Router.js
@@ -1,19 +1,19 @@
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Board from "../routes/Board";
-import Home from "../routes/Home";
-import Login from "../routes/Auth";
-import Navigation from "./Nav";
-import TeacherList from "../routes/TeacherList";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Board from '../routes/Board';
+import Home from '../routes/Home';
+import Auth from '../routes/Auth';
+import Navigation from './Nav';
+import TeacherList from '../routes/TeacherList';
 
 const AppRouter = () => {
   return (
     <Router>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Board" element={<Board />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/TeacherList" element={<TeacherList />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/Board' element={<Board />} />
+        <Route path='/Auth' element={<Auth />} />
+        <Route path='/TeacherList' element={<TeacherList />} />
       </Routes>
     </Router>
   );
