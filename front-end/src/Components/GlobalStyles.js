99ed06b3a22e6e1ca6d1e9a6d2b9cc4349e8d833
거녀리 맥북 <rjsduf@geonyeoliui-MacBookAir.local>
2021-11-11 07:18:08 +0900

    <style> fixed inner class's padding

--- front-end/src/Components/GlobalStyles.js
+++ front-end/src/Components/GlobalStyles.js
@@ -42,7 +42,7 @@ const GlobalStyles = createGlobalStyle`
     .inner {
         width: 100%;
         max-width: 1400px;
-        padding: 40px 80px;
+        padding: 0px 80px;
         margin: 0 auto;
     }
 
