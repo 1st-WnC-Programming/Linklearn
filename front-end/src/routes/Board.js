4fc85909e08bb4a1af5fe936c7f611a86ec2d19f
bae-sh <hyeon1293@gmail.com>
2021-11-11 13:40:24 +0900

    filter title

--- front-end/src/routes/Board.js
+++ front-end/src/routes/Board.js
@@ -1,56 +1,41 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-const info = [
-  {
-    title: '공지사항 입니다.공지사항 입니다.공지사항 입니다.',
-    date: '2021-07-23',
-    views: '42',
-    id: 0,
-  },
-  { title: '공지사항 입니다1.', date: '2021-07-23', views: '42', id: 1 },
-  { title: '공지사항 입니다2.', date: '2021-07-23', views: '42', id: 2 },
-  { title: '공지사항 입니다3.', date: '2021-07-23', views: '42', id: 3 },
-];
-const dataFile = [
-  { title: '데이터 파일0', date: '2021-07-23', views: '24', id: 4 },
-  { title: '데이터 파일1', date: '2021-07-23', views: '24', id: 5 },
-  { title: '데이터 파일2', date: '2021-07-23', views: '24', id: 6 },
-  { title: '데이터 파일3', date: '2021-07-23', views: '24', id: 7 },
-];
+import { useState } from 'react';
+
 const Container = styled.div`
   display: flex;
   flex: 1;
 `;
 const SearchBox = styled.div`
   margin: auto;
-  width: 90%;
-  height: 50px;
-  background-color: lightblue;
-  margin-bottom: 50px;
+  width: 1000px;
+  height: 55px;
+  margin: 50px auto;
   padding: 10px;
   display: flex;
+  justify-content: center;
 `;
-const SortTitle = styled.div`
+const SortTitle = styled.select`
   background-color: #f9f9f9;
-  width: 20%;
+  width: 100px;
   height: 100%;
   line-height: 30px;
   margin-right: 10px;
 `;
 const SeacrchInput = styled.input`
   background-color: #f9f9f9;
-  width: 60%;
+  width: 600px;
   height: 100%;
-  line-height: 30px;
   border: 0px;
   margin-right: 10px;
+  border: 1px solid black;
 `;
 const SearchButton = styled.button`
-  width: 20%;
-  background-color: #f9f9f9;
+  width: 150px;
+  background-color: #3c78c0;
 `;
 const Table = styled.table`
-  width: 100%;
+  width: 1500px;
   border: 1px solid #ededed;
 `;
 const Row = styled.tr`
@@ -74,29 +59,159 @@ const Row = styled.tr`
     }
   }
 `;
-const contentList = () => {
-  const result = [];
-  for (let i = 0; i < 5; i++) {
-    result.push(
-      <Row>
-        <td>{i + 1}</td>
-        <td id='title'>제목입니다</td>
-        <td>그룹</td>
-        <td>3</td>
-        <td>2</td>
-        <td>한석원</td>
-        <td>21.11.11</td>
-      </Row>,
-    );
-  }
-  return result;
-};
+const AddBoardListButton = styled.button`
+  background-color: #3c78c0;
+  border-radius: 10px;
+  margin: 30px auto;
+  width: 100%;
+  height: 50px;
+  font-size: 18px;
+`;
+const ButtonBox = styled.div`
+  margin: auto;
+  width: 200px;
+`;
+
 const Board = () => {
+  const [info, setInfo] = useState([
+    { title: '데이터 파일0', date: '2021-07-23', views: '24', id: 4 },
+    { title: '데이터 파일1', date: '2021-07-23', views: '24', id: 5 },
+    { title: '데이터 파일2', date: '2021-07-23', views: '24', id: 6 },
+    { title: '데이터 파일3', date: '2021-07-23', views: '24', id: 7 },
+  ]);
+
+  const [dataFile, setDataFile] = useState([
+    {
+      number: '1',
+      title: '가',
+      type: '그룹',
+      numberOfPeople: '3',
+      time: '2',
+      teacher: '한석원',
+      date: '2021-07-23',
+    },
+    {
+      number: '2',
+      title: '나',
+      type: '그룹',
+      numberOfPeople: '3',
+      time: '2',
+      teacher: '한석원',
+      date: '2021-07-23',
+    },
+    {
+      number: '3',
+      title: '다',
+      type: '개인',
+      numberOfPeople: '3',
+      time: '2',
+      teacher: '한석원',
+      date: '2021-07-23',
+    },
+    {
+      number: '4',
+      title: '제목입니다',
+      type: '그룹',
+      numberOfPeople: '3',
+      time: '2',
+      teacher: '배성현',
+      date: '2021-07-23',
+    },
+    {
+      number: '5',
+      title: '제목입니다',
+      type: '그룹',
+      numberOfPeople: '3',
+      time: '2',
+      teacher: '전병민',
+      date: '2021-07-23',
+    },
+    {
+      number: '6',
+      title: '제목입니다',
+      type: '그룹',
+      numberOfPeople: '3',
+      time: '2',
+      teacher: '한석원',
+      date: '2021-07-23',
+    },
+  ]);
+  const [keyword, setKeyword] = useState(null);
+  const selectList = {
+    all: '전체',
+    title: '제목',
+    type: '과외 종류',
+    teacher: '선생님',
+    numberOfPeople: '모집인원',
+  };
+  const [selected, setSelected] = useState('all');
+  const [viewingListCount, setViewingListCount] = useState(5);
+  const selectHandler = (e) => {
+    e.preventDefault();
+    setSelected(e.target.value);
+  };
+  const searchSpace = async (e) => {
+    let search = await e.target.value;
+    setKeyword(search);
+  };
+  const contentList = (viewingListCount) => {
+    const result = [];
+    const filterData = dataFile.filter((item) => {
+      if (keyword == null || keyword == '') return item;
+      else {
+        if ((selected == 'all' || selected === 'title') && item.title.includes(keyword)) {
+          return item;
+        } else if ((selected == 'all' || selected == 'teacher') && item.teacher.includes(keyword)) {
+          return item;
+        } else if (
+          (selected == 'all' || selected == 'numberOfPeople') &&
+          item.numberOfPeople.includes(keyword)
+        )
+          return item;
+        else if ((selected == 'all' || selected == 'type') && item.type.includes(keyword)) return item;
+      }
+    });
+    for (let i = 0; i < viewingListCount; i++) {
+      if (filterData.length <= i) break;
+      let curData = filterData[i];
+      result.push(
+        <Row>
+          <td>{curData.number}</td>
+          <td id='title'>
+            <Link
+              to={{
+                pathname: '/Board/' + curData.number,
+                state: { isInfo: false },
+              }}
+            >
+              {curData.title}
+            </Link>
+          </td>
+          <td>{curData.type}</td>
+          <td>{curData.numberOfPeople}</td>
+          <td>{curData.time}</td>
+          <td>{curData.teacher}</td>
+          <td>{curData.date}</td>
+        </Row>,
+      );
+    }
+    return result;
+  };
   return (
     <div className='inner'>
       <SearchBox>
-        <SortTitle>123</SortTitle>
-        <SeacrchInput></SeacrchInput>
+        <SortTitle onChange={selectHandler} value={selected}>
+          {Object.entries(selectList).map((item) => (
+            <option value={item[0]} key={item[0]}>
+              {item[1]}
+            </option>
+          ))}
+        </SortTitle>
+        <SeacrchInput
+          type='text'
+          placeholder='검색어를 입력하세요.'
+          onChange={(e) => searchSpace(e)}
+        ></SeacrchInput>
         <SearchButton>검색</SearchButton>
       </SearchBox>
       <Container>
@@ -112,9 +227,18 @@ const Board = () => {
               <th>등록일</th>
             </Row>
           </thead>
-          <tbody>{contentList()}</tbody>
+          <tbody>{contentList(viewingListCount)}</tbody>
         </Table>
       </Container>
+      <ButtonBox>
+        <AddBoardListButton
+          onClick={() => {
+            setViewingListCount(viewingListCount + 5);
+          }}
+        >
+          더보기
+        </AddBoardListButton>
+      </ButtonBox>
     </div>
   );
 };
