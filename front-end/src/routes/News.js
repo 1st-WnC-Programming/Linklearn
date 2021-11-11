ce4f6b8a8317c7772f030845c9fe36a7229228d2
bae-sh <hyeon1293@gmail.com>
2021-11-11 18:28:16 +0900

    modify News

--- front-end/src/routes/News.js
+++ front-end/src/routes/News.js
@@ -6,6 +6,7 @@ import { Link, useParams } from 'react-router-dom';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 
 import ReactMarkdown from 'react-markdown';
+import { useState } from 'react';
 
 const TitleBox = styled.div`
   width: 1000px;
@@ -36,34 +37,55 @@ const Content = styled.div`
   min-height: 500px;
   /* background-color: red; */
 `;
+const viewMode = (id, title, setEdit) => {
+  return (
+    <>
+      <TitleBox>
+        <h1>{title}</h1>
+      </TitleBox>
+      <Content>{id}</Content>
+      <ButtonBox>
+        <Link
+          to={{
+            pathname: '/Board/',
+          }}
+        >
+          <PostButton>취소</PostButton>
+        </Link>
+        <PostButton onClick={() => setEdit(false)}>취소</PostButton>
+        <PostButton onClick={() => setEdit(false)}>수정</PostButton>
+      </ButtonBox>
+    </>
+  );
+};
+const editMode = (title, setEdit) => {
+  return (
+    <>
+      <TitleBox>
+        <Title placeholder={title}></Title>
+      </TitleBox>
+      <Editor
+        previewStyle='vertical'
+        height='400px'
+        initialEditType='markdown'
+        useCommandShortcut={true}
+        initialValue='마크다운으로 내용을 입력하세요.'
+      />
+      <ButtonBox>
+        <PostButton onClick={() => setEdit(false)}>취소</PostButton>
+        <PostButton>삭제</PostButton>
+        <PostButton>수정</PostButton>
+      </ButtonBox>
+    </>
+  );
+};
 const News = () => {
   const { id } = useParams();
+  const [edit, setEdit] = useState(true);
+  const [title, setTitle] = useState('제목입니다');
   return (
     <div className='inner'>
-      <main>
-        <TitleBox>
-          <h1>제목입니다</h1>
-        </TitleBox>
-        {/* <Editor
-          previewStyle='vertical'
-          height='400px'
-          initialEditType='markdown'
-          useCommandShortcut={true}
-          initialValue='마크다운으로 내용을 입력하세요.'
-        /> */}
-        <Content>{id}</Content>
-        <ButtonBox>
-          <Link
-            to={{
-              pathname: '/Board/',
-              state: { isInfo: false },
-            }}
-          >
-            <PostButton>취소</PostButton>
-          </Link>
-          <PostButton>수정</PostButton>
-        </ButtonBox>
-      </main>
+      <main>{edit === false ? viewMode(title, setEdit) : editMode(title, setEdit)}</main>
     </div>
   );
 };
