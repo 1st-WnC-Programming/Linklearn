084ebb73c1aad1ef8d80173f98ad56025edd2d37
거녀리 맥북 <rjsduf@geonyeoliui-MacBookAir.local>
2021-11-12 19:09:48 +0900

    <fix> fixed card realtime rendering

--- front-end/src/Components/CardProfile.js
+++ front-end/src/Components/CardProfile.js
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const CardProfile = ({ data, target, keyword }) => {
+  const [card, setCard] = useState('');
+  // console.log(data);s
   let count = 0;
-  const cardList = data
-    .filter((item) => {
-      if (keyword == null || keyword == '' || target === 'none') return item;
-      else {
-        if (target === 'name' && item.name.toLowerCase().includes(keyword.toLowerCase())) {
-          return item;
-        } else if (target == 'field' && item.field.toLowerCase().includes(keyword.toLowerCase())) {
-          return item;
-        } else if (target == 'career' && item.career.toLowerCase().includes(keyword.toLowerCase())) {
-          return item;
-        }
+  const newData = data.filter((item) => {
+    if (keyword == null || keyword == '' || target === 'none') return item;
+    else {
+      if (target === 'name' && item.name.toLowerCase().includes(keyword.toLowerCase())) {
+        return item;
+      } else if (target == 'field' && item.field.toLowerCase().includes(keyword.toLowerCase())) {
+        return item;
+      } else if (target == 'career' && item.career.toLowerCase().includes(keyword.toLowerCase())) {
+        return item;
       }
-    })
-    .map((value) => (
-      <div className='cardItem innerContainer' key={count++ + value.name + value.point}>
-        <div className='innerItem'>{value.image}</div>
-        <div className='innerItem'>
-          <div>{value.name} 선생님</div>
-          <div>분야 : {value.field}</div>
-          <div>별점 : {value.starPoint}</div>
-          <div>경력 : {value.career}</div>
+    }
+  });
+
+  useEffect(() => {
+    setCard(
+      newData.map((value) => (
+        <div className='cardItem innerContainer' key={count++ + value.name + value.starPoint}>
+          <div className='innerItem'>{value.image}</div>
+          <div className='innerItem'>
+            <div>{value.name} 선생님</div>
+            <div>분야 : {value.field}</div>
+            <div>별점 : {value.starPoint}</div>
+            <div>경력 : {value.career}</div>
+          </div>
         </div>
-      </div>
-    ));
-  return <div className='cardContainer'>{cardList}</div>;
+      )),
+    );
+  }, [newData]);
+  // const cardList = data
+  //   .filter((item) => {
+  //     if (keyword == null || keyword == '' || target === 'none') return item;
+  //     else {
+  //       if (target === 'name' && item.name.toLowerCase().includes(keyword.toLowerCase())) {
+  //         return item;
+  //       } else if (target == 'field' && item.field.toLowerCase().includes(keyword.toLowerCase())) {
+  //         return item;
+  //       } else if (target == 'career' && item.career.toLowerCase().includes(keyword.toLowerCase())) {
+  //         return item;
+  //       }
+  //     }
+  //   })
+  //   .map((value) => (
+  //     <div className='cardItem innerContainer' key={count++ + value.name + value.starPoint}>
+  //       <div className='innerItem'>{value.image}</div>
+  //       <div className='innerItem'>
+  //         <div>{value.name} 선생님</div>
+  //         <div>분야 : {value.field}</div>
+  //         <div>별점 : {value.starPoint}</div>
+  //         <div>경력 : {value.career}</div>
+  //       </div>
+  //     </div>
+  //   ));
+  return <div className='cardContainer'>{card}</div>;
 };
 
 export default CardProfile;
